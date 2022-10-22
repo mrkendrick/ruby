e9723791aabf7fa9199f997e7e41121d2aa7ff93
Charles Duruaku <charlesrichson@gmail.com>
2022-10-22 18:46:55 +0100

    method

---
+++ methods.rb
@@ -0,0 +1,14 @@
+# A method os a block of code that contains specific logic
+# Methods will automatically return the last evalutaed statement
+
+def multiply(first_number, second_number)
+  first_number * second_number
+end
+
+multiply(23, 67)
+
+def calc_speed(distance, time)
+  distance / time.to_f
+end
+
+calc_speed(100, 9.78)
