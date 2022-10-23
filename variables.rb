f181899598c0f662dc90a8e71dd0fbbe69071482
Charles Duruaku <charlesrichson@gmail.com>
2022-10-24 00:19:28 +0100

    variables

---
+++ variables.rb
@@ -0,0 +1,12 @@
+name = 'Charles' # local variable
+
+$global_variable = 10 # global variable
+
+class Person
+  @@no_of_students = 100 # class variable
+
+  def initialize(name, age)
+    @name = name # instance variable
+    @age = age
+  end
+end
