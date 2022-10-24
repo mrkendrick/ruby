ff3f157e781ec5ade17474dcb3f14b0963cd757b
Charles Duruaku <charlesrichson@gmail.com>
2022-10-24 23:51:24 +0100

    update

--- main.rb
+++ main.rb
@@ -13,3 +13,21 @@ def say_hello(thing_to_say)
 end
 
 say_hello 'Hello world'
+
+print "What's your first name? "
+first_name = gets.chomp
+first_name.capitalize!
+
+print 'What is your last name? '
+last_name = gets.chomp
+last_name.capitalize!
+
+print "What's your city? "
+city = gets.chomp
+city.capitalize!
+
+print "What's your state? "
+state = gets.chomp
+state.upcase!
+
+puts "#{first_name} #{last_name} #{city} #{state}"
