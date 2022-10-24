7c54b6922c6da3a5aea7910e7db903a6d15f67d4
Charles Duruaku <charlesrichson@gmail.com>
2022-10-24 23:51:16 +0100

    dir

---
+++ directory.rb
@@ -0,0 +1,14 @@
+# creating directory
+# puts Dir.mkdir('folder')
+
+# checking if the directory exists or not
+puts Dir.exist?('folder')
+
+# checking if the directory is empty or not
+puts Dir.empty?('folder')
+
+# returns current working directory
+puts Dir.pwd
+
+# returns home directory
+puts Dir.home
